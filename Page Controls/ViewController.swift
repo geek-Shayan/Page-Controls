d7fda28b6a632132ecfb5c8116d7256344377ed8
SHAYANUL HAQ SADI <shayanul.haq@nexdecade.com>
2024-01-10 15:37:30 +0600

    Initial Commit

---
+++ Page Controls/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Page Controls
+//
+//  Created by SHAYANUL HAQ SADI on 1/10/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
