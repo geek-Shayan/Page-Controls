0a98c9f6eccfa85e16d19ecd385a2bcab4848b06
SHAYANUL HAQ SADI <shayanul.haq@nexdecade.com>
2024-02-29 11:26:18 +0600

    moddified

--- Page Controls/ViewController.swift
+++ Page Controls/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var button: UIButton!
+    
+    @IBOutlet weak var pageControl: AdvancedPageControlView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        pageControl.drawer = ExtendedDotDrawer(numberOfPages: 5,
+                                                space: 16.0,
+                                                indicatorColor: UIColor.green,
+                                                dotsColor: .gray,
+                                                isBordered: true,
+                                                borderWidth: 0.0,
+                                                indicatorBorderColor: .blue,
+                                                indicatorBorderWidth: 0.0)
     }
 
+    var value = 0
 
+    @IBAction func buttonTapped(_ sender: Any) {
+        
+//        let value = Int.random(in: 0 ... 4)
+        
+        if value < 4 {
+            value += 1
+            pageControl.setPage( value )
+        }
+        else {
+            value = 0
+            pageControl.setPage( value )
+        }
+        
+    }
 }
 
